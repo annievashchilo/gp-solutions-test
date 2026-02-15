afcddd255ceaa91b3a2e5fbd3a5e98b2ff930b30
Anna Vashchilo <melanthium@gmail.com>
2026-02-15 17:59:20 +0300

    added main runner for debugging

---
+++ propertyview/src/main/java/propertyview/Main.java
@@ -0,0 +1,18 @@
+package propertyview;
+
+import java.util.List;
+import propertyview.dto.HotelDTO;
+import propertyview.repository.JsonFileHotelRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        var repo = new JsonFileHotelRepository();
+        repo.deleteAll();
+
+        var hotels = List.of(new HotelDTO("Hotel A"), new HotelDTO("Hotel B"));
+        hotels.forEach(hotel -> repo.create(hotel));
+
+        hotels = repo.getAll();
+        hotels.forEach(hotel -> System.out.println(hotel.getName()));
+    }
+}
